92fd7dde370a69475c57d652cb4578909a6679a7
flavius841 <flaviusepintilie@gmail.com>
2026-02-11 16:40:11 +0200

    debugging

--- Assets/Resources/Scripts/DesktopPet.cs
+++ Assets/Resources/Scripts/DesktopPet.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Collections;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class DesktopPet : MonoBehaviour
 {
 #if UNITY_STANDALONE_WIN
-    // --- WINDOWS API DEFINITIONS (Only compiles for Windows Builds) ---
     [DllImport("user32.dll")]
     private static extern IntPtr GetActiveWindow();
 
@@ -15,11 +15,8 @@ public class DesktopPet : MonoBehaviour
     [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
     private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
 
-    [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
-    private static extern uint GetWindowLong32(IntPtr hWnd, int nIndex);
-
-    [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
-    private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
@@ -29,10 +26,13 @@ public class DesktopPet : MonoBehaviour
     const int GWL_EXSTYLE = -20;
     const uint WS_EX_LAYERED = 0x80000;
     const uint WS_EX_TRANSPARENT = 0x20;
-    const int GWL_STYLE = -16;
-    const uint WS_POPUP = 0x80000000;
-    const uint WS_VISIBLE = 0x10000000;
-    
+
+    // Always On Top
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    const uint SWP_NOSIZE = 0x0001;
+    const uint SWP_NOMOVE = 0x0002;
+    const uint SWP_SHOWWINDOW = 0x0040;
+
     private IntPtr hWnd;
 #endif
 
@@ -41,65 +41,78 @@ public class DesktopPet : MonoBehaviour
     void Start()
     {
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
-        // 1. Get Window Handle
         hWnd = GetActiveWindow();
 
-        // 2. Make Background Transparent (The Glass Effect)
+        // Start coroutine to repeatedly apply transparency
+        StartCoroutine(ApplyTransparencyRepeatedly());
+
+        // Force Always On Top
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0,
+            SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+#endif
+    }
+
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+    IEnumerator ApplyTransparencyRepeatedly()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ApplyTransparency();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void ApplyTransparency()
+    {
+        // Glass effect
         MARGINS margins = new MARGINS { cxLeftWidth = -1 };
         DwmExtendFrameIntoClientArea(hWnd, ref margins);
 
-        // 3. Set Initial Window Style (Layered so we can control transparency)
-        // We start nicely clickable
+        // Layered style
         SetWindowExStyle(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
-        
-        // Optional: Force removal of borders if Unity didn't do it
-        // SetWindowExStyle(hWnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-#endif
     }
+#endif
 
     void Update()
     {
-        // --- LOGIC THAT WORKS IN EDITOR (FOR TESTING) ---
-        // We still run the raycast so you can debug "hitting" the pet in Linux
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         bool hitPet = Physics.Raycast(ray, out RaycastHit hit);
 
-        // --- WINDOWS-ONLY LOGIC (THE MAGIC) ---
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
         if (hitPet && !isClickable)
         {
-            SetClickable(true); // Enable Clicking
+            SetClickable(true);
         }
         else if (!hitPet && isClickable)
         {
-            SetClickable(false); // Enable Click-Through (Ghost Mode)
+            SetClickable(false);
         }
 #endif
 
-        // Fun Test: Click to jump (Works in Editor too!)
+        // Jump logic
         if (hitPet && Input.GetMouseButtonDown(0))
         {
-            if (hit.rigidbody != null) hit.rigidbody.AddForce(Vector3.up * 300);
+            if (hit.rigidbody != null)
+                hit.rigidbody.AddForce(Vector3.up * 300);
         }
     }
 
-    // --- HELPER FUNCTION TO SWITCH MODES ---
     private void SetClickable(bool clickable)
     {
         isClickable = clickable;
+
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
-        // If clickable, we just want Layered.
-        // If NOT clickable (ghost), we want Layered + Transparent.
-        uint flags = clickable ? WS_EX_LAYERED : (WS_EX_LAYERED | WS_EX_TRANSPARENT);
-        
-        if (IntPtr.Size == 8) // 64-bit
+        uint flags = clickable
+            ? WS_EX_LAYERED
+            : (WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+        if (IntPtr.Size == 8)
             SetWindowLongPtr64(hWnd, GWL_EXSTYLE, new IntPtr(flags));
-        else // 32-bit
+        else
             SetWindowLong32(hWnd, GWL_EXSTYLE, flags);
 #endif
     }
 
-    // Helper to abstract 32/64 bit differences
     private void SetWindowExStyle(IntPtr hWnd, int nIndex, uint flags)
     {
 #if UNITY_STANDALONE_WIN && !UNITY_EDITOR
